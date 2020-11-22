f3b6994c21be778ffe7953389749c657af8198c5
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-11-22 12:13:47 +0600

    Home screen created

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import HomeScreen from './src/screens/HomeScreen';
+
 
 const HomeStack = createStackNavigator();
 
 function App() {
   return (
     <HomeStack.Navigator initialRouteName="Home">
+      <HomeStack.Screen name="Home" component={HomeScreen} options={{ title: "Home Page" }}></HomeStack.Screen>
     </HomeStack.Navigator>
   );
 }
