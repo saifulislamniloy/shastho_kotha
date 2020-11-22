6e60ca28ea13c0a649b099b3409636e2bfa3fc54
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-11-22 12:39:35 +0600

    Button added in home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,53 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Button, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
     return (
         <View>
-            <Text>Home Screen</Text>
-        </View>
+            <View style={styles.layout}>
+                <View style={styles.button}>
+                    <Button title="BMI" />
+                </View>
+                <View style={styles.button}>
+                    <Button title="Body Fat" />
+                </View>
+            </View>
+            <View style={styles.layout}>
+                <View style={styles.button}>
+                    <Button title="Food Nutrition" />
+                </View>
+                <View style={styles.button}>
+                    <Button title="Water Notification" />
+                </View>
+            </View>
+            <View style={styles.layout}>
+                <View style={styles.button}>
+                    <Button title="Exercise Helper" />
+                </View>
+                <View style={styles.button}>
+                    <Button title="Meal Planner" />
+                </View>
+            </View>
+            <View style={styles.layout}>
+                <View style={styles.button}>
+                    <Button title="Goal Set & Check" />
+                </View>
+            </View>
+        </View >
     );
 }
+
+const styles = StyleSheet.create({
+    layout:{ 
+        flex: 1, 
+        flexDirection: "row", 
+        justifyContent: "center" 
+    },
+    button: {
+        flex: 0.5,
+        margin: 10,
+        padding: 10
+    }
+})
 
 export default HomeScreen;
